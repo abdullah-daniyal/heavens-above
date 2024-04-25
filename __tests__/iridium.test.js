a703da986cafb54256fdca2b28620425fcfeac54
abdullah-daniyal <abdullah-daniyal@users.noreply.github.com>
2024-04-25 12:40:49 +0000

    deploy: b560467f4376375cfa2445f1bd0d5b9d83fc7d0c

---
+++ __tests__/iridium.test.js
@@ -0,0 +1,8 @@
+const iridium = require("../src/iridium");
+
+describe("iridium", () => {
+  test("functions correctly", () => {
+    // Replace true with actual tests for your functions
+    expect(true).toBeTruthy();
+  });
+});
---
+++ __tests__/satellite.test.js
@@ -0,0 +1,8 @@
+const satellite = require("../src/satellite");
+
+describe("satellite", () => {
+  test("functions correctly", () => {
+    // Replace true with actual tests for your functions
+    expect(true).toBeTruthy();
+  });
+});
