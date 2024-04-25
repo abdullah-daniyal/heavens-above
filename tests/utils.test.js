1083361ad102a2726a37fecc4a06bce4883cbd6c
abdullah-daniyal <abdullah-daniyal@users.noreply.github.com>
2024-04-25 12:34:29 +0000

    deploy: 5ceb0a56564b19c10baf76758324818f76fc118f

---
+++ tests/utils.test.js
@@ -0,0 +1,8 @@
+const utils = require("../src/utils");
+
+describe("utils", () => {
+  test("functions correctly", () => {
+    // Replace true with actual tests for your functions
+    expect(true).toBeTruthy();
+  });
+});
